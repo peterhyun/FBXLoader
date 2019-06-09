22ba56cbf334fd4b6791daff22ba3bc454c6fe1f
jeehoonhyun <peterpan1998@snu.ac.kr>
2019-06-09 17:07:28 +0900

    Initial Commit

---
+++ FBXLoader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FBXLoader
+//
+//  Created by Jeehoon Hyun on 09/06/2019.
+//  Copyright © 2019 Jeehoon Hyun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
